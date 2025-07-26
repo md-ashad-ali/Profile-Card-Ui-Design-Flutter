28ac86c0fb2fdd7f7c60902ff75d3957a9702e35
CodeWithAshad <ashad.appdev@gmail.com>
2025-07-26 23:45:19 +0600

    profile Card ui Design V 1.0.0

---
+++ android/app/src/main/java/com/example/profilecardui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.profilecardui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Main/HomePage.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+     var mqheight = MediaQuery.of(context).size.height;
+     var mqweight = MediaQuery.of(context).size.width;
+
+ final CustomGradent = LinearGradient(
+   begin: Alignment.topLeft,
+     end: Alignment.bottomLeft,
+   colors: [
+     const Color(0xFFFED9FF).withOpacity(0.5),
+     const Color(0xFFFF6EB9).withOpacity(1.00)
+   ]
+ );
+    return Scaffold(
+
+      body: Container(
+        width: mqweight,
+        height: mqheight,
+        decoration: BoxDecoration(
+          gradient: CustomGradent
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            SizedBox(height: 40,),
+
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(120),
+                    color: Colors.white
+                  ),
+                ),
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('images/bosprofile.jpg'),fit: BoxFit.cover)
+                  ),
+                ),
+
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text('Md Ashad Ali',style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+            ),),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text('ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old',style: TextStyle(
+                color: Colors.black,
+                fontSize: 13,
+                fontWeight: FontWeight.w400,
+
+              ),
+              textAlign: TextAlign.center,),
+            ),
+            SizedBox(height: 90,),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                      child:Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF030C47),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Center(child: Text('Following',style: TextStyle(color: Colors.white))),
+                      )
+                  ),
+
+                  SizedBox(width: 15,),
+
+                  Expanded(
+                      child:Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Color(0xFF030C47),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Center(child: Text('Profile Edit',style: TextStyle(color: Colors.white))),
+                      )
+                  ),
+                ],
+              ),
+            )
+
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
